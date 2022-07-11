586ab2f9901903dfcd612bc87311c54711154eb3
Kartavyag5 <kartavyag5@gmail.com>
2022-07-11 17:04:27 +0530

    first commit

---
+++ constants/data.js
@@ -0,0 +1,105 @@
+const userData = {
+  githubUsername: "Kartavyag5",
+  name: "Kartavya Gauswami",
+  designation: "React Developer",
+  avatarUrl: "/avatar.png",
+  email: "kartavyag5@gmail.com",
+  phone: "+91 7874792504",
+  address: "Dahod, Gujarat, India.",
+  projects: [
+    {
+      title: "",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a ReactJS developer & also Python Backend developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since last two years. `,
+      `My first programming language is python, so I completed internship for the role of python web dev, then join as a professional web programmer in Creole Ventures PVT LTD(Ahmedabad).`,
+      `Then I start Learning Reactjs as FrontEnd Developer, recently I completed three projects as a ReactJS & NextJS developer.`,
+      `As a Developer i continuously learning new trends & technologies`,
+      `Always trying to make myself better than what yesterday it was`
+    ],
+    currentProject: "Innerbody-Research-NextJS",
+    currentProjectUrl: "https://www.innerbody.com/health-products-services#hearing",
+  },
+  experience: [
+    {
+      title: "ReactJs Developer",
+      company: "Creole Ventures PVT LTD(Ahmedabad)",
+      year: "2022",
+      companyLink: "https://www.creolestudios.com/",
+      desc: "Contributed to three dedicated projects as ReactJS & NextJS Developer",
+    },
+    {
+      title: "Python Web Developer",
+      company: "Creole Studios",
+      year: "2021",
+      companyLink: "https://www.creolestudios.com/",
+      desc: "Developed a backend Database for market Survey App using Python (flask, Sqlite, postgresDB ,HTML, CSS, JS)",
+    },
+    {
+      title: "Internship",
+      company: "Creole Studios",
+      year: "2020",
+      companyLink: "https://www.creolestudios.com/",
+      desc: "Developed APIs for the Ecommerce Website with JWT Authentication & Razorpay Integration.",
+    },
+    {
+      title: "Graduation",
+      company: "Government Engineering College, Dahod.",
+      year: "2015-2019",
+      companyLink: "http://www.gecdh.cteguj.in/",
+      desc: "Bachelor of Technology in Computer Engineering with a CGPA of 6.9. Nobody asks this but it's okay.",
+    }
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1k7Pdr8X3VwZibUjLLJ8XOW6oLN6pI0U7/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/kartavyag5",
+    linkedin: "https://www.linkedin.com/mwlite/in/kartavya-gauswami-87435b1a1",
+    github: "https://github.com/Kartavyag5",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
